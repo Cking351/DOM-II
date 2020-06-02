d96d3d8f858c75c2503ce6217b3b4af95fa04fd7
Chris King <cking5836@gmail.com>
2020-06-02 17:03:25 -0400

    8 events created

--- js/index.js
+++ js/index.js
@@ -25,7 +25,7 @@ bottomButton.addEventListener('mouseout', goodbyeColor)
 document.addEventListener('keydown', logKey)
 
 function logKey(event) {
-    alert('You broke FunBus :(')
+    alert('You broke Fun Bus :(')
 }
 
 // dbl Click
@@ -34,3 +34,31 @@ const imageCard = document.querySelector('img');
 imageCard.addEventListener('dblclick', function (event) {
     imageCard.classList.toggle('large')
 })
+
+// Select Cant seem to make this work??
+function mustStop(event) {
+    const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd)
+    alert('You are stealing from Fun Bus')
+}
+
+const stopStealing = document.querySelector('p')
+stopStealing.addEventListener('select', mustStop)
+
+// Mouse enter
+const h1Title = document.querySelector('h1');
+
+function changeTheTitle(event) {
+    event.target.style.color = 'red'
+}
+h1Title.addEventListener('mouseenter', changeTheTitle)
+
+// Mouse Down
+const h2Title = document.querySelector('h2');
+
+function changeH2Title(event) {
+    event.target.style.color = 'white'
+    alert('There never was an h2 here. Never will be....unless you refresh the page')
+}
+h2Title.addEventListener('mousedown', changeH2Title)
+
+//
